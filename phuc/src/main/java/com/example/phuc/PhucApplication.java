fa041ffa8e845374f06d20946dba01c1603f5905
nam <nampqhe173479@fpt.edu.vn>
2025-05-18 17:39:07 +0700

    demo

---
+++ phuc/src/main/java/com/example/phuc/PhucApplication.java
@@ -0,0 +1,13 @@
+package com.example.phuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhucApplication.class, args);
+    }
+
+}
---
+++ phuc/src/test/java/com/example/phuc/PhucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phuc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
